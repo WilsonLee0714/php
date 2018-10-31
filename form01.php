d661db1e4d6da71a8d571fbc5cf162fc292813b8
Wilson <wilsonlee0714@gmail.com>
2018-10-31 11:39:02 +0800

    form begin

---
+++ form01.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+if(isset($_POST['email'])):
+    print_r($_POST);
+else:
+?>
+<form name="form1" action="" method="post">
+    <label for="email">Email</label>
+    <input type="text" id="email" name="email">
+    <br>
+    <label for="password">password</label>
+    <input type="password" id="password" name="password">
+    <br>
+    <input type="submit">
+</form>
+<?php
+endif;
+?>
+</body>
+</html>
