33a3a10b56bc14439f64cebaab0ff5ba2b603ffb
你çWilson Lee <wilsonlee0714@gmail.com>
2018-11-05 12:05:01 +0800

    page list

--- ab_list.php
+++ ab_list.php
@@ -5,14 +5,30 @@ $pname = 'list'; // 自訂的頁面名稱
 $per_page = 5; // 每頁有幾筆
 $page = isset($_GET['page']) ? intval($_GET['page']) : 1; // 第幾頁
 
-$sql = sprintf("SELECT * FROM address_book LIMIT %s, %s",
-        ($page-1)*$per_page, $per_page);
+$t_sql = "SELECT COUNT(1) FROM address_book";
+$total_rows = $pdo->query($t_sql)->fetch()[0]; // 總筆數
+$total_pages = ceil($total_rows/$per_page); // 總頁數
 
+$sql = sprintf("SELECT * FROM address_book LIMIT %s, %s",
+    ($page-1)*$per_page, $per_page);
 $stmt = $pdo->query($sql);
 ?>
 <?php include __DIR__. '/__html_head.php'; ?>
 <?php include __DIR__. '/__navbar.php'; ?>
-<div class="container" style="margin-top: 20px">
+<div class="container" style="...">
+<div><?= $total_rows. '::'. $total_pages ?></div>
+    <nav aria-label="Page navigation example">
+        <ul class="pagination">
+            <!-- <li class="page-item"><a class="page-link" href="#">Previous</a></li> -->
+            <?php for($i=1; $i<=$total_pages; $i++):?>
+            <li class="page-item <?= $i==$page ? 'active' : ''; ?>">
+                    <a class="page-link" href="?page=<?=$i?>"><?=$i?></a>
+            </li>
+            <?php endfor ?>
+            <!-- <li class="page-item"><a class="page-link" href="#">Next</a></li> -->
+        </ul>
+    </nav>
+
     <table class="table table-striped table-bordered">
         <thead>
         <tr>
