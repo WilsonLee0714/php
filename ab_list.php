f0cc9901fd0361466f1317f4e9a3d9c0beb705d1
你çWilson Lee <wilsonlee0714@gmail.com>
2018-11-05 10:43:55 +0800

    page block

---
+++ ab_list.php
@@ -0,0 +1,34 @@
+<?php
+require __DIR__. '/__connect_db.php';
+
+$stmt = $pdo->query("SELECT * FROM address_book");
+?>
+<?php include __DIR__. '/__html_head.php'; ?>
+<?php include __DIR__. '/__navbar.php'; ?>
+<div class="container" style="margin-top: 20px">
+    <table class="table table-striped table-bordered">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">name</th>
+            <th scope="col">email</th>
+            <th scope="col">mobile</th>
+            <th scope="col">address</th>
+            <th scope="col">birthday</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php while($r = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
+        <tr>
+            <th scope="row"><?= $r['sid'] ?></th>
+            <td><?= $r['name'] ?></td>
+            <td><?= $r['email'] ?></td>
+            <td><?= $r['mobile'] ?></td>
+            <td><?= $r['address'] ?></td>
+            <td><?= $r['birthday'] ?></td>
+        </tr>
+        <?php endwhile; ?>
+        </tbody>
+    </table>
+</div>
+<?php include __DIR__. '/__html_foot.php';
